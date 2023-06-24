2bff01528517d1188badb67d56e1cb1a0f116a17
shiroichi28 <manikandanmk2972@gmail.com>
2023-06-24 23:02:24 +0530

    updated comments

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,22 +1,29 @@
+// Define the possible values for a player
 type Player = "X" | "O" | null | string;
+
+// Square component represents an individual square on the board
 const Square = ({
   value,
   onClick,
   winner,
 }: {
-  winner: Player;
-  value: Player;
-  onClick: () => void;
+  winner: Player; // The winner of the game
+  value: Player; // The value of the square ("X", "O", or null)
+  onClick: () => void; // Function to handle click event
 }) => {
+  // If the square has no value (not filled), render a button that can be clicked
   if (!value) {
     return (
       <button className="square" onClick={onClick} disabled={Boolean(winner)} />
     );
   }
+
+  // If the square has a value, render a disabled button with appropriate styling
   return (
     <button className={`square square_${value.toLowerCase()}`} disabled>
       {value}
     </button>
   );
 };
+
 export default Square;
